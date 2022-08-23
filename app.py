db6707c36e293e98221b145f4a302978f14ed41b
mel291295 <102040009+mel291295@users.noreply.github.com>
2022-08-23 16:14:41 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+#!Python
+#importing the libraries
+import streamlit as st
+
+
+#main function
+def main():
+  st.title("Once upon a time")
+  st.write("Summary, Translation and Text Preprocessing of Fairy Tales")
